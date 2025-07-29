31dcf773f36cdd5f0dcb32dbd0de58d4d1e8afd1
Ronald Estes <64745255+rgechef@users.noreply.github.com>
2025-07-29 19:17:22 -0400

    Update deploy_agent.py

--- deploy_agent.py
+++ deploy_agent.py
@@ -1,11 +1,13 @@
 import os
+from dotenv import load_dotenv
 import subprocess
 
-RENDER_DEPLOY_HOOK = os.environ.get("RENDER_DEPLOY_HOOK")
-VERCEL_DEPLOY_HOOK = os.environ.get("VERCEL_DEPLOY_HOOK")
+load_dotenv()  # This will load variables from .env in the same folder
 
-def deploy():
-    if RENDER_DEPLOY_HOOK:
-        subprocess.run(["curl", "-X", "POST", RENDER_DEPLOY_HOOK])
-    if VERCEL_DEPLOY_HOOK:
-        subprocess.run(["curl", "-X", "POST", VERCEL_DEPLOY_HOOK])
+vercel_hook = os.environ['VERCEL_DEPLOY_HOOK']
+render_hook = os.environ['RENDER_DEPLOY_HOOK']
+
+# Deploy to Vercel
+subprocess.run(f'curl --ssl-no-revoke -X POST "{vercel_hook}"', shell=True)
+# Deploy to Render
+subprocess.run(f'curl --ssl-no-revoke -X POST "{render_hook}"', shell=True)
