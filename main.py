411d751ed11588baa78c10d17e7fe4e2a01249ba
Ronald Estes <64745255+rgechef@users.noreply.github.com>
2025-07-29 20:14:05 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
+from fastapi.middleware.cors import CORSMiddleware
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # For dev only. For production, use your frontend domain
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 from fastapi import FastAPI
 from pydantic import BaseModel
 
